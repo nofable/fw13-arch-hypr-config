8b58e8c8844d1f04eb2513d35ea27213f71d8f0c
nofable <6097010+nofable@users.noreply.github.com>
2025-12-18 19:35:46 +0000

    tidied up bootstrap script and added gst alias

--- bash/.bashrc
+++ bash/.bashrc
@@ -7,6 +7,7 @@
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
+alias gst='git status'
 PS1='[\u@\h \W]\$ '
 
 eval "$(starship init bash)"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
+set -euo pipefail
 
-# Install Pacman packages
-sudo pacman -S --needed - <~/.dotfiles/pkglist.txt
+DOTFILES="$HOME/.dotfiles"
+
+# Install pacman packages (regex filters out comments and empty lines)
+sudo pacman -S --needed $(grep -vE '^\s*#|^\s*$' "$DOTFILES/pkglist.txt")
 
 # Install yay
 if ! command -v yay >/dev/null 2>&1; then
-  git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-  cd ~ && rm -rf yay
+  tmpdir=$(mktemp -d)
+  git clone https://aur.archlinux.org/yay.git "$tmpdir/yay"
+  pushd "$tmpdir/yay"
+  makepkg -si
+  popd
+  rm -rf "$tmpdir"
 fi
 
-# Create .stowrc so we don't need to add dir parameter
-if [ -f ~/.stowrc ]; then
-  echo "~/.stowrc already exists." >&1
-else
-  echo "--dir=~/.dotfiles" >~/.stowrc
+# Setup .stowrc
+STOWRC="$HOME/.stowrc"
+if [ ! -e "$STOWRC" ]; then
+  echo "--dir=$DOTFILES" >"$STOWRC"
 fi
 
-# Setup stow packages
-stow bash
-stow fonts
-stow hypr
-stow kitty
-stow nvim
-stow starship
-stow waybar
-stow yay
-stow yazi
-stow chromium
+# Backup .bashrc if needed
+BASHRC="$HOME/.bashrc"
+if [ -L "$BASHRC" ]; then
+  echo "$BASHRC is already a symlink; assuming stow-managed"
+elif [ -e "$BASHRC" ]; then
+  BACKUP="$HOME/.bashrc.pre-stow.$(date +%Y%m%d%H%M%S)"
+  echo "Moving $BASHRC to $BACKUP"
+  mv "$BASHRC" "$BACKUP"
+fi
+
+# Stow all packages
+for pkg in bash fonts hypr kitty nvim starship waybar yazi chromium; do
+  stow "$pkg"
+done
